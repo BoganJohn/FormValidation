5c4ad8ebcfe6a8569cc0099cebf5d0b0336c49ef
BoganJohn <74276347+BoganJohn@users.noreply.github.com>
2021-02-08 14:42:47 -0600

    Initial commit

---
+++ src/test/java/tts/com/FormValidation/FormValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package tts.com.FormValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
